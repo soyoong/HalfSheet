03661fb35d21c21b868d23b8891edfd43dbf93f5
AnthonyCode274 <63950809+AnthonyCode274@users.noreply.github.com>
2022-10-06 11:12:10 +0700

    Update public level

--- Sources/HalfSheet/HalfSheet.swift
+++ Sources/HalfSheet/HalfSheet.swift
@@ -10,22 +10,22 @@ import UIKit
 
 @available(iOS 13.0, *)
 @available(iOS 15.0, *)
-extension View {
-    func halfSheet<Content : View> (isPresented: Binding<Bool>, onDismiss: (() -> Void)? = nil, @ViewBuilder content: @escaping () -> Content) -> some View {
-        modifier(HalfSheetViewModifier(isPresented: isPresented, onDismiss: onDismiss, contentView: content))
+public extension View {
+    static func halfSheet<Content : View> (isPresented: Binding<Bool>, onDismiss: (() -> Void)? = nil, @ViewBuilder content: @escaping () -> Content) -> some View {
+        HalfSheetView(isPresented: isPresented, onDismiss: onDismiss, content: content)
     }
 }
 
 // 1 - Create a UISheetPresentationController that can be used in a SwiftUI interface
 @available(iOS 13.0, *)
 @available(iOS 15.0, *)
-struct HalfSheet<Content: View>: UIViewRepresentable {
+public struct HalfSheet<Content: View>: UIViewRepresentable {
     
-    @Binding var isPresented: Bool
-    let onDismiss: (() -> Void)?
-    let content: Content
+    @Binding private var isPresented: Bool
+    private let onDismiss: (() -> Void)?
+    private let content: Content
     
-    init
+    public init
     (
         isPresented: Binding<Bool>,
         onDismiss: (() -> Void)? = nil,
@@ -37,12 +37,12 @@ struct HalfSheet<Content: View>: UIViewRepresentable {
         self.content = content()
     }
     
-    func makeUIView(context: Context) -> UIView {
+    public func makeUIView(context: Context) -> UIView {
         let view = UIView()
         return view
     }
     
-    func updateUIView(_ uiView: UIView, context: Context) {
+    public func updateUIView(_ uiView: UIView, context: Context) {
         
         // Create the UIViewController that will be presented by the UIButton
         let vc = UIViewController()
@@ -77,7 +77,7 @@ struct HalfSheet<Content: View>: UIViewRepresentable {
         vc.presentationController?.delegate = context.coordinator
         
         
-        if isPresented {
+        if self.isPresented {
             // Present the viewController
             uiView.window?.rootViewController?.present(vc, animated: true)
         } else {
@@ -90,15 +90,15 @@ struct HalfSheet<Content: View>: UIViewRepresentable {
     /* Creates the custom instance that you use to communicate changes
      from your view controller to other parts of your SwiftUI interface.
      */
-    func makeCoordinator() -> Coordinator {
+    public func makeCoordinator() -> Coordinator {
         Coordinator(isPresented: $isPresented, onDismiss: onDismiss)
     }
     
-    class Coordinator: NSObject, UISheetPresentationControllerDelegate {
-        @Binding var isPresented: Bool
-        let onDismiss: (() -> Void)?
+    public class Coordinator: NSObject, UISheetPresentationControllerDelegate {
+        @Binding private var isPresented: Bool
+        private let onDismiss: (() -> Void)?
         
-        init
+        public init
         (
             isPresented: Binding<Bool>,
             onDismiss: (() -> Void)? = nil)
@@ -107,7 +107,7 @@ struct HalfSheet<Content: View>: UIViewRepresentable {
             self.onDismiss = onDismiss
         }
         
-        func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
+        public func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
             self.isPresented = false
             self.onDismiss?()
         }
@@ -117,77 +117,41 @@ struct HalfSheet<Content: View>: UIViewRepresentable {
 // 2 - Create the SwiftUI modifier conforming to the ViewModifier protocol
 @available(iOS 13.0, *)
 @available(iOS 15.0, *)
-struct HalfSheetViewModifier<ContentView : View>: ViewModifier {
+public struct HalfSheetView<Content : View>: View {
     
-    @Binding var isPresented: Bool
+    @Binding private var isPresented: Bool
     
-    let onDismiss: (() -> Void)?
+    private  let onDismiss: (() -> Void)?
     
-    let contentView: ContentView
+    private let content: Content
     
-    init(isPresented: Binding<Bool>,
-         onDismiss: (() -> Void)? = nil,
-         @ViewBuilder contentView: @escaping () -> ContentView)
+    public init
+    (
+        isPresented: Binding<Bool>,
+        onDismiss: (() -> Void)? = nil,
+        @ViewBuilder content: @escaping () -> Content)
     {
         self._isPresented = isPresented
         self.onDismiss = onDismiss
-        self.contentView = contentView()
+        self.content = content()
     }
     
-    func body(content: Content) -> some View {
-        content
+    public var body: some View {
+        self.content
             .overlay(
                 HalfSheet(isPresented: $isPresented, onDismiss: onDismiss) {
-                    self.contentView
-                }.fixedSize()
+                    self.content
+                }
             )
     }
-}
-
-
-// 4 - An example of how to use the sheetWithDetents modifier
-@available(iOS 13.0, *)
-@available(iOS 15.0, *)
-struct DemoHalfSheet: View {
-    @State var isPresented: Bool = false
-    @State var state: UISheetPresentationController.Detent.Identifier = .medium
     
-    var body: some View {
-        ZStack {
-            Color.pink
-                .ignoresSafeArea()
-            Button {
-                isPresented.toggle()
-            } label: {
-                VStack {
-                    Text("Tap me!")
-                        .padding()
-                        .background(Color.white)
-                        .cornerRadius(20)
-                }
-                .onChange(of: self.state) { _ in
-                    print("\(self.state.rawValue)")
-                }
-            }
-            .halfSheet(isPresented: $isPresented, onDismiss: onDismiss) {
-                VStack {
-                    Text("SwiftUI Content ")
-                        .bold()
-                        .font(.title)
-                }
-            }
-        }
-    }
-    
-    func onDismiss() {
-        print("The sheet has been dismissed")
-    }
+//    public func body: some View {
+//        content
+//            .overlay(
+//                HalfSheet(isPresented: $isPresented, onDismiss: onDismiss) {
+//                    self.contentView
+//                }.fixedSize()
+//            )
+//    }
 }
 
-@available(iOS 13.0, *)
-@available(iOS 15.0, *)
-struct HalfSheet_Previews: PreviewProvider {
-    static var previews: some View {
-        DemoHalfSheet()
-    }
-}
